8c58ddcea278b61a28b689e22f7573b550fb96eb
Eric Liu <ericyl.us@gmail.com>
2020-07-31 05:04:08 -0700

    chore(deps-dev): bump dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 import pkg from "./package.json";
 
 const IS_PROD = !process.env.ROLLUP_WATCH;
@@ -11,7 +10,6 @@ export default (async () => {
       output: { file: pkg.browser, format: "umd", name: pkg.name },
       plugins: [
         resolve(),
-        commonjs(),
         IS_PROD && (await import("rollup-plugin-terser")).terser(),
       ],
     },
@@ -21,7 +19,7 @@ export default (async () => {
         { file: pkg.main, format: "cjs" },
         { file: pkg.module, format: "es" },
       ],
-      plugins: [resolve(), commonjs()],
+      plugins: [resolve()],
     },
   ];
 })();
