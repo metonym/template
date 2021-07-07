d6a32e03cd0fee3e1013d03d9c4feefb14ffdd47
Eric Y Liu <ericyl.us@gmail.com>
2021-07-07 14:16:05 -0700

    chore(deps-dev): bump deps

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,7 @@ import { mdsvex } from "mdsvex";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ".md"],
-  preprocess: [
-    mdsvex({
-      extensions: [".md"],
-    }),
-  ],
+  preprocess: [mdsvex({ extensions: [".md"] })],
   kit: {
     target: "#svelte",
     adapter: adapter(),
