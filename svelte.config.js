fa9907d2a3a12a4dedcad00af8b9ddabab98a538
Eric Liu <ericyl.us@gmail.com>
2022-01-29 11:11:26 -0800

    Add component name alias

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
+import fs from "fs";
+import path from "path";
 import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
+
+const pkg = JSON.parse(fs.readFileSync(new URL('package.json', import.meta.url), 'utf8'));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,6 +19,11 @@ const config = {
       template: "demo/_app.html",
     },
     vite: {
+      resolve: {
+        alias: {
+          [pkg.name]: path.resolve('./src')
+        }
+      },
       server: {
         fs: {
           allow: [".."],
