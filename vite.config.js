fc93ce005a2d9135b514174fd2db8984cdb0a612
Eric Liu <ericyl.us@gmail.com>
2022-02-06 11:48:11 -0800

    Use vite-plugin-gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,26 @@
 import pkg from "./package.json";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
+import { ghPages } from "vite-plugin-gh-pages";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      svelte({
-        extensions: [".svelte"],
-        preprocess: [
-          {
-            script: ({ content }) => {
-              return {
-                code: content.replace(
-                  /process.env.VERSION/g,
-                  JSON.stringify(pkg.version)
-                ),
-              };
-            },
+export default defineConfig({
+  base: "/repo-name/",
+  plugins: [
+    svelte({
+      extensions: [".svelte"],
+      preprocess: [
+        {
+          script: ({ content }) => {
+            return {
+              code: content.replace(
+                /process.env.VERSION/g,
+                JSON.stringify(pkg.version)
+              ),
+            };
           },
-        ],
-      }),
-    ],
-  };
+        },
+      ],
+    }),
+    ghPages(),
+  ],
 });
