6422f3a7e3627317633ba9ef85e93b9911ad1e0d
Eric Liu <ericyl.us@gmail.com>
2022-07-19 11:07:15 -0700

    chore(types): update utility type

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,9 +2,10 @@ import type { Element } from "svelte/types/compiler/interfaces";
 import type { PreprocessorGroup } from "svelte/types/compiler/preprocess";
 
 declare global {
-  type PreprocessorType = keyof PreprocessorGroup;
-
-  interface SveltePreprocessor<PreprocessorType, Options = any> {
+  export interface SveltePreprocessor<
+    PreprocessorType extends keyof PreprocessorGroup,
+    Options = any
+  > {
     (options?: Options): Required<Pick<PreprocessorGroup, PreprocessorType>>;
   }
 
