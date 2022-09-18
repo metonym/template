b6364a2e0f7820344942bff4c7ddaad21c7f7482
Eric Liu <ericyl.us@gmail.com>
2022-09-18 12:17:37 -0700

    feat: add `@astrojs/prefetch`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [svelte(), sitemap()],
-  trailingSlash: "always",
+  integrations: [svelte(), sitemap(), prefetch()],
+  trailingSlash: "always"
 });
