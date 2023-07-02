198e4c5562373ea49fd835285ef6d7254088bc99
Eric Liu <ericyl.us@gmail.com>
2023-07-02 13:21:40 -0700

    refactor: extend `Element` for custom selectors

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,27 +1,14 @@
 declare module "svelte/compiler" {
   import type { Ast, Element } from "svelte/types/compiler/interfaces";
-  interface ClassSelectorNode {
-    type: "ClassSelector";
-    name: string;
-    start: number;
-    end: number;
-    children: any[];
-  }
 
-  interface SelectorNode {
-    type: "Selector";
-    name: string;
-    start: number;
-    end: number;
-    children: ClassSelectorNode[];
+  interface AugmentedElement extends Element {
+    type: "ClassSelector" | "InlineComponent";
   }
-
-  type AstNode = Element | ClassSelectorNode | SelectorNode;
 
   export function walk(
     ast: Ast,
     options: {
-      enter: (node: AstNode, parentNode: AstNode) => void;
+      enter: (node: AugmentedElement, parentNode: AugmentedElement) => void;
     }
   ): void;
 }
