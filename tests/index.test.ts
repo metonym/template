f2e004ca56fc8e601662002fb0faee3983859d17
metonym <ericyl.us@gmail.com>
2023-09-13 14:03:34 -0700

    Use Bun

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,14 +1,6 @@
 import type { Processed } from "svelte/types/compiler/preprocess";
-import { expect, test } from "vitest";
+import { expect, test } from "bun:test";
 import * as API from "../src";
-
-test("API", () => {
-  expect(Object.keys(API)).toMatchInlineSnapshot(`
-    [
-      "preprocessor",
-    ]
-  `);
-});
 
 const preprocess = (content: string) =>
   (API.preprocessor().markup?.({ content }) as Processed).code;
