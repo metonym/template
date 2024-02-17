72ece67ed5fdc12f034af8c51d5d7d5d899870d5
Eric Liu <ericyl.us@gmail.com>
2024-02-17 10:49:17 -0800

    feat: add csrf middleware

--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,10 @@
 import { trpcServer } from "@hono/trpc-server";
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
+import { csrf } from "hono/csrf";
 import { logger } from "hono/logger";
 import { secureHeaders } from "hono/secure-headers";
-import { NODE_ENV, PORT } from "./constants";
+import { NODE_ENV, PORT, SITE_URL } from "./constants";
 import { router } from "./trpc-router";
 
 const app = new Hono();
@@ -11,6 +12,7 @@ const app = new Hono();
 if (NODE_ENV === "production") {
   app
     .use(secureHeaders())
+    .use(csrf({ origin: new URL(SITE_URL).host }))
     .use(logger())
     .use("/*", serveStatic({ root: "./dist" }));
 }
