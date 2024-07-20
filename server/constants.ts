79f7e7571da10775e2cd2d4a2fff7bbff2f589f4
Eric Liu <ericyl.us@gmail.com>
2024-07-19 17:04:33 -0700

    chore(deps-dev): bump deps

--- server/constants.ts
+++ server/constants.ts
@@ -1,8 +1,3 @@
-/**
- * Bun automatically parses and injects environment variables from the .env file.
- * @see https://bun.sh/docs/runtime/env
- */
-
 export const PORT = import.meta.env.PORT ?? 3000;
 export const SITE_URL = import.meta.env.SITE_URL ?? "http://localhost:3000/";
-export const NODE_ENV = import.meta.env.NODE_ENV;
+export const NODE_ENV = import.meta.env.NODE_ENV ?? "development";
